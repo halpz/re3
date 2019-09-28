5854016ec51458711eacc7b1ac1fa415ee9db979
Sergeanur <s.anureev@yandex.ua>
2019-09-28 21:39:58 +0300

    PlayerSkin

--- src/core/PlayerInfo.cpp
+++ src/core/PlayerInfo.cpp
@@ -4,9 +4,9 @@
 #include "PlayerInfo.h"
 #include "Frontend.h"
 #include "Vehicle.h"
+#include "PlayerSkin.h"
 
 WRAPPER void CPlayerInfo::MakePlayerSafe(bool) { EAXJMP(0x4A1400); }
-WRAPPER void CPlayerInfo::LoadPlayerSkin() { EAXJMP(0x4A1700); }
 WRAPPER void CPlayerInfo::AwardMoneyForExplosion(CVehicle *vehicle) { EAXJMP(0x4A15F0); }
 WRAPPER void CPlayerInfo::Process(void) { EAXJMP(0x49FD30); }
 
@@ -22,3 +22,25 @@ CVector& CPlayerInfo::GetPos()
 		return m_pPed->m_pMyVehicle->GetPosition();
 	return m_pPed->GetPosition();
 }
+
+void CPlayerInfo::LoadPlayerSkin()
+{
+	DeletePlayerSkin();
+
+	m_pSkinTexture = CPlayerSkin::GetSkinTexture(m_aSkinName);
+	if (!m_pSkinTexture)
+		m_pSkinTexture = CPlayerSkin::GetSkinTexture(DEFAULT_SKIN_NAME);
+}
+
+void CPlayerInfo::DeletePlayerSkin()
+{
+	if (m_pSkinTexture) {
+		RwTextureDestroy(m_pSkinTexture);
+		m_pSkinTexture = NULL;
+	}
+}
+
+STARTPATCHES
+InjectHook(0x4A1700, &CPlayerInfo::LoadPlayerSkin, PATCH_JUMP);
+InjectHook(0x4A1750, &CPlayerInfo::DeletePlayerSkin, PATCH_JUMP);
+ENDPATCHES
