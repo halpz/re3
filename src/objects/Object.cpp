a2b89c6a18d43910e9626c23793a4935e2c0d08d
aap <aap@papnet.eu>
2020-05-06 17:13:46 +0200

    sorted out object collision enums

--- src/objects/Object.cpp
+++ src/objects/Object.cpp
@@ -173,12 +173,12 @@ CObject::ObjectDamage(float amount)
 		const float fDirectionZ = 0.0002f * amount;
 		switch (m_nCollisionDamageEffect)
 		{
-		case COLLRESPONSE_CHANGE_MODEL: 
+		case DAMAGE_EFFECT_CHANGE_MODEL: 
 			bRenderDamaged = true;
 			break;
-		case COLLRESPONSE_SPLIT_MODEL:
+		case DAMAGE_EFFECT_SPLIT_MODEL:
 			break;
-		case COLLRESPONSE_SMASH_COMPLETELY:
+		case DAMAGE_EFFECT_SMASH_COMPLETELY:
 			bIsVisible = false;
 			bUsesCollision = false;
 			bIsStatic = true;
@@ -186,7 +186,7 @@ CObject::ObjectDamage(float amount)
 			SetMoveSpeed(0.0f, 0.0f, 0.0f);
 			SetTurnSpeed(0.0f, 0.0f, 0.0f);
 			break;
-		case COLLRESPONSE_CHANGE_THEN_SMASH:
+		case DAMAGE_EFFECT_CHANGE_THEN_SMASH:
 			if (!bRenderDamaged) {
 				bRenderDamaged = true;
 			}
@@ -199,7 +199,7 @@ CObject::ObjectDamage(float amount)
 				SetTurnSpeed(0.0f, 0.0f, 0.0f);
 			}
 			break;
-		case COLLRESPONSE_SMASH_CARDBOARD_COMPLETELY: {
+		case DAMAGE_EFFECT_SMASH_CARDBOARD_COMPLETELY: {
 			bIsVisible = false;
 			bUsesCollision = false;
 			bIsStatic = true;
@@ -222,7 +222,7 @@ CObject::ObjectDamage(float amount)
 			PlayOneShotScriptObject(_SCRSOUND_CARDBOARD_BOX_SMASH, vecPos);
 			break;
 		}
-		case COLLRESPONSE_SMASH_WOODENBOX_COMPLETELY: {
+		case DAMAGE_EFFECT_SMASH_WOODENBOX_COMPLETELY: {
 			bIsVisible = false;
 			bUsesCollision = false;
 			bIsStatic = true;
@@ -245,7 +245,7 @@ CObject::ObjectDamage(float amount)
 			PlayOneShotScriptObject(_SCRSOUND_WOODEN_BOX_SMASH, vecPos);
 			break;
 		}
-		case COLLRESPONSE_SMASH_TRAFFICCONE_COMPLETELY: {
+		case DAMAGE_EFFECT_SMASH_TRAFFICCONE_COMPLETELY: {
 			bIsVisible = false;
 			bUsesCollision = false;
 			bIsStatic = true;
@@ -270,7 +270,7 @@ CObject::ObjectDamage(float amount)
 			PlayOneShotScriptObject(_SCRSOUND_TYRE_BUMP, vecPos);
 			break;
 		}
-		case COLLRESPONSE_SMASH_BARPOST_COMPLETELY: {
+		case DAMAGE_EFFECT_SMASH_BARPOST_COMPLETELY: {
 			bIsVisible = false;
 			bUsesCollision = false;
 			bIsStatic = true;
