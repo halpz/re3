8e0b132f43b0ac952255bf85b0d1e22e3879d694
Nikolay Korolev <nickvnuk@gmail.com>
2020-05-14 14:03:56 +0300

    it works differently

--- src/audio/sampman_oal.cpp
+++ src/audio/sampman_oal.cpp
@@ -779,10 +779,10 @@ cSampleManager::IsPedCommentLoaded(uint32 nComment)
 
 	for ( int32 i = 0; i < _TODOCONST(3); i++ )
 	{
-#ifdef FIX_BUGS
-		slot = (nCurrentPedSlot - i - 1) % ARRAY_SIZE(nPedSlotSfx);
-#else
 		slot = nCurrentPedSlot - i - 1;
+#ifdef FIX_BUGS
+		if (slot < 0)
+			slot += ARRAY_SIZE(nPedSlotSfx);
 #endif
 		if ( nComment == nPedSlotSfx[slot] )
 			return true;
