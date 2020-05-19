62db8cd9b0030f042403e996b20c3ceca5cd4f7a
aap <aap@papnet.eu>
2020-05-19 10:23:08 +0200

    finished CFileLoader; some COcclusion stubs

--- src/render/Occlusion.h
+++ src/render/Occlusion.h
@@ -1,7 +1,26 @@
 #pragma once
 
+class COccluder
+{
+public:
+	int16 width, length, height;
+	int16 x, y, z;
+	uint16 angle;
+	int16 listIndex;
+};
+
 class COcclusion
 {
 public:
+	static int32 NumOccludersOnMap;
+	static int16 FarAwayList;
+	static int16 NearbyList;
+	static int16 ListWalkThroughFA;
+	static int16 PreviousListWalkThroughFA;
+
+	static COccluder aOccluders[NUMOCCLUSIONVOLUMES];
+
+	static void Init(void);
+	static void AddOne(float x, float y, float z, float width, float length, float height, float angle);
 	static void ProcessBeforeRendering(void);
 };
