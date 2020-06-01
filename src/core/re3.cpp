bfd13d58e077ffdb1ed95351480f9a5ab361a61d
aap <aap@papnet.eu>
2020-06-01 22:02:22 +0200

    add freeroam; clean up debug menu

--- src/core/re3.cpp
+++ src/core/re3.cpp
@@ -386,25 +386,13 @@ DebugMenuPopulate(void)
 		DebugMenuAddCmd("Debug", "Toggle Comedy Controls", ToggleComedy);
 		DebugMenuAddCmd("Debug", "Place Car on Road", PlaceOnRoad);
 
-		DebugMenuAddVarBool8("Debug", "Catalina Heli On", &CHeli::CatalinaHeliOn, nil);
-		DebugMenuAddCmd("Debug", "Catalina Fly By", CHeli::StartCatalinaFlyBy);
-		DebugMenuAddCmd("Debug", "Catalina Take Off", CHeli::CatalinaTakeOff);
-		DebugMenuAddCmd("Debug", "Catalina Fly Away", CHeli::MakeCatalinaHeliFlyAway);
 		DebugMenuAddVarBool8("Debug", "Script Heli On", &CHeli::ScriptHeliOn, nil);
-
-#ifdef TOGGLEABLE_BETA_FEATURES
-		DebugMenuAddVarBool8("Debug", "Toggle popping heads on headshot", &CPed::bPopHeadsOnHeadshot, nil);
-		DebugMenuAddVarBool8("Debug", "Toggle peds running to phones to report crimes", &CPed::bMakePedsRunToPhonesToReportCrimes, nil);
-#endif
 
 		DebugMenuAddCmd("Debug", "Start Credits", CCredits::Start);
 		DebugMenuAddCmd("Debug", "Stop Credits", CCredits::Stop);
 
 #ifdef RELOADABLES
 		DebugMenuAddCmd("Reload", "HUD.TXD", CHud::ReloadTXD);
-		DebugMenuAddCmd("Reload", "FONTS.TXD", NULL);
-		DebugMenuAddCmd("Reload", "FRONTEN1.TXD", NULL);
-		DebugMenuAddCmd("Reload", "FRONTEN2.TXD", NULL);
 #endif
 
 		extern bool PrintDebugCode;
