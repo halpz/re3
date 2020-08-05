b73b6b8e130a545687aad4024465fc977e3f01ee
majestic <majesticcoding@gmail.com>
2020-08-04 19:11:42 -0700

    CWindModifiers

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -89,6 +89,7 @@
 #include "Occlusion.h"
 #include "debugmenu.h"
 #include "Ropes.h"
+#include "WindModifiers.h"
 
 eLevelName CGame::currLevel;
 int32 CGame::currArea;
@@ -693,6 +694,7 @@ void CGame::Process(void)
 	if (!CCutsceneMgr::IsCutsceneProcessing() && !CTimer::GetIsCodePaused())
 		FrontEndMenuManager.Process();
 	CStreaming::Update();
+	CWindModifiers::Number = 0;
 	if (!CTimer::GetIsPaused())
 	{
 		CTheZones::Update();
