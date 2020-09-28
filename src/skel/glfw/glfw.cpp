d05c50ea743f42bc9c426b7b6c2f9598b8bc7f94
Greg V <greg@unrelenting.technology>
2020-09-28 04:50:57 +0300

    Use glfwSetFramebufferSizeCallback instead of glfwSetWindowSizeCallback
    Framebuffer size is scaled by the display scale.
    This fixes the game being shrunk to the bottom left quarter of the window on Wayland HiDPI setups.
    
    Corresponding change in librw: glfwGetWindowSize -> glfwGetFramebufferSize.

--- src/skel/glfw/glfw.cpp
+++ src/skel/glfw/glfw.cpp
@@ -917,7 +917,7 @@ void psPostRWinit(void)
 	RwEngineGetVideoModeInfo(&vm, GcurSelVM);
 
 	glfwSetKeyCallback(PSGLOBAL(window), keypressCB);
-	glfwSetWindowSizeCallback(PSGLOBAL(window), resizeCB);
+	glfwSetFramebufferSizeCallback(PSGLOBAL(window), resizeCB);
 	glfwSetScrollCallback(PSGLOBAL(window), scrollCB);
 	glfwSetCursorPosCallback(PSGLOBAL(window), cursorCB);
 	glfwSetCursorEnterCallback(PSGLOBAL(window), cursorEnterCB);
