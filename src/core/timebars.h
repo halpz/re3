73e4350320a11b0f3ca7530cedc9364363c2ce03
Filip Gawin <filip.gawin@zoho.com>
2020-09-29 17:27:36 +0200

    Use Const instead of const (last commit)

--- src/core/timebars.h
+++ src/core/timebars.h
@@ -1,6 +1,6 @@
 #pragma once
 
 void tbInit();
-void tbStartTimer(int32, const char*);
-void tbEndTimer(const char*);
+void tbStartTimer(int32, Const char*);
+void tbEndTimer(Const char*);
 void tbDisplay();
