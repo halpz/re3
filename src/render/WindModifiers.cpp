d1394c69946032f40109bc12ece5549a93dfde79
Sergeanur <s.anureev@yandex.ua>
2020-10-24 22:28:44 +0300

    Fix WindModifiers

--- src/render/WindModifiers.cpp
+++ src/render/WindModifiers.cpp
@@ -33,7 +33,7 @@ CWindModifiers::FindWindModifier(CVector pos, float *x, float *y)
 				float dist = (pos - Array[i].m_pos).Magnitude();
 				if (dist < MAX_FADE_DIST) {
 					float distFade = dist < MIN_FADE_DIST ? 1.0f : 1.0f - (dist - MIN_FADE_DIST) / (MAX_FADE_DIST - MIN_FADE_DIST);
-					float heightFade = 1.0f - zDist / MAX_HEIGHT_DIST;
+					float heightFade = distFade * ((1.0f - zDist / MAX_HEIGHT_DIST) / 2.0f);
 					dir = (pos - Array[i].m_pos) * heightFade / dist;
 					bWasWindModifierFound = true;
 				}
