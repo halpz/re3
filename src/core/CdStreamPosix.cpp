28fb26f79233157c961a1b4e1c8f2286e570c5df
Adrian Graber <adrian@adriangraber.com>
2020-10-24 16:12:10 +0200

    Join CdStream threads to wait for them to exit

--- src/core/CdStreamPosix.cpp
+++ src/core/CdStreamPosix.cpp
@@ -216,10 +216,12 @@ CdStreamShutdown(void)
 #ifndef ONE_THREAD_PER_CHANNEL
 	gCdStreamThreadStatus = 2;
 	sem_post(gCdStreamSema);
+	pthread_join(_gCdStreamThread, nil);
 #else
 	for ( int32 i = 0; i < gNumChannels; i++ ) {
 		gpReadInfo[i].nThreadStatus = 2;
 		sem_post(gpReadInfo[i].pStartSemaphore);
+		pthread_join(gpReadInfo[i].pChannelThread, nil);
 	}
 #endif
 }
