e9735f928f2c7c0675ef8ac2881da380f390576e
withmorten <morten.with@gmail.com>
2020-11-01 01:09:46 +0100

    add ifdef LIBRW guard for pipelines and colourfilter

--- src/core/config.h
+++ src/core/config.h
@@ -227,8 +227,10 @@ enum Config {
 #define NO_ISLAND_LOADING  // disable loadscreen between islands via loading all island data at once, consumes more memory and CPU
 //#define USE_TEXTURE_POOL
 #define CUTSCENE_BORDERS_SWITCH
+#ifdef LIBRW
 //#define EXTENDED_COLOURFILTER		// more options for colour filter (replaces mblur)
 //#define EXTENDED_PIPELINES		// custom render pipelines (includes Neo)
+#endif
 #define MULTISAMPLING		// adds MSAA option
 
 #ifdef LIBRW
