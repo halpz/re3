cebf32c46c43b55caa96ada3dece3473b6db7785
Roman Masanin <36927roma@gmail.com>
2020-11-01 23:38:45 +0300

    mark code

--- src/audio/AudioLogic.cpp
+++ src/audio/AudioLogic.cpp
@@ -2686,7 +2686,7 @@ cAudioManager::ProcessVehicleOneShots(cVehicleParams *params)
 				break;
 			}
 			m_sQueueSample.m_nBankIndex = SFX_BANK_0;
-			m_sQueueSample.m_nCounter = event + 10;
+			m_sQueueSample.m_nCounter = event + 10; //also used m_asAudioEntities[m_sQueueSample.m_nEntityIndex].m_awAudioEvent[i]
 			if (params->m_pVehicle->GetVehicleAppearance() == VEHICLE_APPEARANCE_HELI)
 				m_sQueueSample.m_nFrequency = 23459;
 			else
@@ -2715,7 +2715,7 @@ cAudioManager::ProcessVehicleOneShots(cVehicleParams *params)
 			const float SOUND_INTENSITY = 35.0f;
 			static uint8 WheelIndex = 82;
 			maxDist = SQR(SOUND_INTENSITY);
-			if (event == SOUND_CAR_JUMP_2) {
+			if (event == SOUND_CAR_JUMP_2) { //also used m_asAudioEntities[m_sQueueSample.m_nEntityIndex].m_awAudioEvent[i]
 				m_sQueueSample.m_nSampleIndex = SFX_TYRE_BURST_B;
 				emittingVol = Max(50.0f, 2 * (60.0f * m_asAudioEntities[m_sQueueSample.m_nEntityIndex].m_afVolume[i]));
 			} else {
