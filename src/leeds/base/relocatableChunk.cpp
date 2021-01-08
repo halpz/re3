a871da4fbe3094a74295b5c00089bc24334ead56
Sergeanur <s.anureev@yandex.ua>
2021-01-08 23:42:42 +0200

    Add Relocatable Chunk stubs

---
+++ src/leeds/base/relocatableChunk.cpp
@@ -0,0 +1,36 @@
+#include "common.h"
+#include "relocatableChunk.h"
+
+namespace base
+{
+	// TODO(LCS): add actual code (all of these are stubs)
+
+	void* cRelocatableChunk::Load(void* data, bool bShrink) { return nil; }
+	void* cRelocatableChunk::Load(const char* name, bool bShrink) { return nil; }
+	void cRelocatableChunk::Fixup(const sChunkHeader& header, void* data) {}
+	void cRelocatableChunk::Fixup(void* data) {}
+	void* cRelocatableChunk::Shrink(const sChunkHeader& header, void* data) { return nil; }
+	void* cRelocatableChunk::Shrink(void* data) { return nil; }
+
+	cRelocatableChunkClassInfo::cRelocatableChunkClassInfo(const char* class_name, const void* pVmt, int size) {}
+
+	cRelocatableChunkWriter::cRelocatableChunkWriter() {}
+	cRelocatableChunkWriter::~cRelocatableChunkWriter() {}
+
+	void cRelocatableChunkWriter::AddPatch(void* addr) {}
+	void cRelocatableChunkWriter::AddPatchWithInfo(const char* str, int unk, void* addr) {}
+	void cRelocatableChunkWriter::AllocateRaw(void* addr, uint32 size, uint32 align, bool a5, bool a6) {}
+
+	void cRelocatableChunkWriter::Clear() {}
+	void cRelocatableChunkWriter::Class(void* ptr, const cRelocatableChunkClassInfo& classInfo) {}
+	void cRelocatableChunkWriter::DebugFileLine(void*) {}
+
+	void cRelocatableChunkWriter::PatchFunc(void* ptr) {}
+
+	bool cRelocatableChunkWriter::IsAllocated(void* addr) { return false; }
+
+	void cRelocatableChunkWriter::Reserve(int, int) {}
+
+	void cRelocatableChunkWriter::Save(const char* filename, uint32 a3, uint32 a4, bool a5) {}
+	void cRelocatableChunkWriter::Save(void* file, uint32 a3, uint32 a4, bool a5, sChunkHeader* pHeader) {}
+};
