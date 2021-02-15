a76c962aa348479ddb5a4d30472183239ddb9285
withmorten <morten.with@gmail.com>
2021-02-15 14:27:35 +0100

    always console for windows Debug builds

--- src/skel/win/win.cpp
+++ src/skel/win/win.cpp
@@ -2019,7 +2019,9 @@ WinMain(HINSTANCE instance,
 	SystemParametersInfo(SPI_SETFOREGROUNDLOCKTIMEOUT, 0, nil, SPIF_SENDCHANGE);
 
 #ifndef MASTER
+#ifndef DEBUG
 	if (strstr(cmdLine, "-console"))
+#endif
 	{
 		AllocConsole();
 		freopen("CONIN$", "r", stdin);
