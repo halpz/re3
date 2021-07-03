060c1181bcf2000a4cea1184d597b3b2c087e447
withmorten <morten.with@gmail.com>
2021-07-03 02:52:54 +0200

    fix previous PED_SKIN commit, add comments

--- src/core/config.h
+++ src/core/config.h
@@ -380,12 +380,12 @@ enum Config {
 #define CAMERA_PICKUP
 
 // Peds
-#define PED_SKIN		// support for skinned geometry on peds
+#define PED_SKIN		// support for skinned geometry on peds, requires COMPATIBLE_SAVES
 #define ANIMATE_PED_COL_MODEL
 // #define VC_PED_PORTS			// various ports from VC's CPed, mostly subtle
 // #define NEW_WALK_AROUND_ALGORITHM	// to make walking around vehicles/objects less awkward
 #define CANCELLABLE_CAR_ENTER
-//#define PEDS_REPORT_CRIMES_ON_PHONE
+//#define PEDS_REPORT_CRIMES_ON_PHONE, requires COMPATIBLE_SAVES
 
 // Camera
 //#define PS2_CAM_TRANSITION	// old way of transitioning between cam modes
@@ -432,7 +432,7 @@ enum Config {
 
 // if these defines are enabled saves are not vanilla compatible without COMPATIBLE_SAVES
 #ifndef COMPATIBLE_SAVES
-#undef PED_SKINS
+#undef PED_SKIN
 #undef PEDS_REPORT_CRIMES_ON_PHONE
 #endif
 
