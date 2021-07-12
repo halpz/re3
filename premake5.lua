b69cee380141896494d18830994615b907ebb0da
withmorten <morten.with@gmail.com>
2021-07-12 23:26:49 +0200

    fix premake

--- premake5.lua
+++ premake5.lua
@@ -303,9 +303,6 @@ project "reVC"
 	includedirs { "src/weapons" }
 	includedirs { "src/extras" }
 
-	filter "configurations:Vanilla"
-		defines { "VANILLA_DEFINES" }
-
 	if(not _OPTIONS["no-git-hash"]) then
 		defines { "USE_OUR_VERSIONING" }
 	end
@@ -315,6 +312,9 @@ project "reVC"
 		includedirs { "vendor/opus/include" }
 		includedirs { "vendor/opusfile/include" }
 	end
+
+	filter "configurations:Vanilla"
+		defines { "VANILLA_DEFINES" }
 
 	filter "platforms:*mss"
 		defines { "AUDIO_MSS" }
